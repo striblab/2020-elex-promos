4682cd2b6369ea53e77f0750f94cab8194ccbb19
Thomas Oide <thomas.t.oide@gmail.com>
2020-03-03 13:04:05 -0600

    test controls and tuesday AM links

--- s3.py
+++ s3.py
@@ -5,11 +5,13 @@ bucketName = os.environ.get('BUCKET_NAME')
 # wireOutput = "elections/projects/2020-election-results/wire.json"
 # localOutput = "elections/projects/2020-election-results/local.json"
 output = "elections/projects/2020-election-results/elex_controls.json"
+outputTest = "elections/projects/2020-election-results/test_controls.json"
 
 # wireFile = './data/wire.json'
 # localFile = './data/local.json'
 
 localFile = './data/elex_controls.json'
+localTest = './data/test_controls.json'
 
 s3 = boto3.client(
     's3',
@@ -17,3 +19,4 @@ s3 = boto3.client(
     aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY_ID')
 )
 s3.upload_file(localFile, bucketName, output)
+s3.upload_file(localTest, bucketName, outputTest)
